a1d10c49ccaa231746a9b68b057453651e13eed1
AMan <35390271+aman4technology@users.noreply.github.com>
2019-10-11 00:33:26 +0800

    style: indent with 4 space

--- Serializer_test.go
+++ Serializer_test.go
@@ -1,44 +1,44 @@
 package serialize
 
 import (
-	"fmt"
-	"reflect"
-	"testing"
+    "fmt"
+    "reflect"
+    "testing"
 
-	"Serialize/codec"
+    "Serialize/codec"
 )
 
 func init() {
-	_ = Register(reflect.TypeOf(my{}), nil, true)
+    _ = Register(reflect.TypeOf(my{}), nil, true)
 }
 
 func TestSerialize(t *testing.T) {
-	fmt.Println(Serialize([]int{1, 2, 3}, codec.String, "data", ""))
-	fmt.Println(Serialize(map[string]int{"a": 1, "b": 2, "c": 3}, codec.String, "data", ""))
-	fmt.Println(Serialize(struct {
-		A string `string:"a"`
-		a int
-	}{A: "wangting", a: 24}, codec.String, "data", "string"))
-	fmt.Println(Serialize(my{A: "WangTing", b: 1}, codec.String, "test", "string"))
+    fmt.Println(Serialize([]int{1, 2, 3}, codec.String, "data", ""))
+    fmt.Println(Serialize(map[string]int{"a": 1, "b": 2, "c": 3}, codec.String, "data", ""))
+    fmt.Println(Serialize(struct {
+        A string `string:"a"`
+        a int
+    }{A: "wangting", a: 24}, codec.String, "data", "string"))
+    fmt.Println(Serialize(my{A: "WangTing", b: 1}, codec.String, "test", "string"))
 }
 
 func TestDeserialize(t *testing.T) {
-	fmt.Println(Deserialize("slice|data|Serialize/internal.StringSlice|sliceData|3|7|int|0|17|int|1|27|int|2|3", codec.String, ""))
-	fmt.Println(Deserialize("map|data|Serialize/internal.StringSlice|mapData|2|71|Serialize/internal.StringSlice|keys|3|9|string||a9|string||b9|string||c64|Serialize/internal.StringSlice|values|3|6|int||16|int||26|int||3", codec.String, ""))
-	fmt.Println(Deserialize("struct|data|Serialize/internal.StringSlice|fields|1|17|string|a|wangting", codec.String, "string"))
-	fmt.Println(Deserialize("Serialize.my|test|Serialize/internal.StringSlice|fields|2|17|string|a|WangTing11|ptr|C|<nil>", codec.String, "string"))
+    fmt.Println(Deserialize("slice|data|Serialize/internal.StringSlice|sliceData|3|7|int|0|17|int|1|27|int|2|3", codec.String, ""))
+    fmt.Println(Deserialize("map|data|Serialize/internal.StringSlice|mapData|2|71|Serialize/internal.StringSlice|keys|3|9|string||a9|string||b9|string||c64|Serialize/internal.StringSlice|values|3|6|int||16|int||26|int||3", codec.String, ""))
+    fmt.Println(Deserialize("struct|data|Serialize/internal.StringSlice|fields|1|17|string|a|wangting", codec.String, "string"))
+    fmt.Println(Deserialize("Serialize.my|test|Serialize/internal.StringSlice|fields|2|17|string|a|WangTing11|ptr|C|<nil>", codec.String, "string"))
 }
 
 func TestIDOf(t *testing.T) {
-	value := reflect.New(reflect.TypeOf(my{}))
-	c := reflect.New(reflect.TypeOf("c"))
-	c.Elem().SetString("test")
-	value.Elem().FieldByName("C").Set(c.Elem().Addr())
-	fmt.Println(*value.Elem().Interface().(my).C)
+    value := reflect.New(reflect.TypeOf(my{}))
+    c := reflect.New(reflect.TypeOf("c"))
+    c.Elem().SetString("test")
+    value.Elem().FieldByName("C").Set(c.Elem().Addr())
+    fmt.Println(*value.Elem().Interface().(my).C)
 }
 
 type my struct {
-	A string `string:"a"`
-	b int
-	C *string
+    A string `string:"a"`
+    b int
+    C *string
 }
--- internal/StringSlice.go
+++ internal/StringSlice.go
@@ -1,15 +1,15 @@
 package internal
 
 import (
-	"reflect"
-	"strconv"
-	"strings"
+    "reflect"
+    "strconv"
+    "strings"
 
-	"Serialize/codec"
+    "Serialize/codec"
 )
 
 func init() {
-	_ = Register(reflect.TypeOf(StringSlice{}), StringSliceSerializer{}, false)
+    _ = Register(reflect.TypeOf(StringSlice{}), StringSliceSerializer{}, false)
 }
 
 const Nil = "nil"
@@ -19,44 +19,44 @@ type StringSlice []string
 type StringSliceSerializer struct{}
 
 func (StringSliceSerializer) Serialize(value interface{}, tag string) (string, error) {
-	stringSlice := value.(StringSlice)
-	if stringSlice == nil {
-		return Nil, nil
-	}
-	var (
-		length = len(stringSlice)
-		data   = strings.Builder{}
-	)
-	data.WriteString(strconv.Itoa(length))
-	data.WriteByte(codec.Split)
-	for _, s := range stringSlice {
-		data.WriteString(strconv.Itoa(len(s)))
-		data.WriteByte(codec.Split)
-		data.WriteString(s)
-	}
-	return data.String(), nil
+    stringSlice := value.(StringSlice)
+    if stringSlice == nil {
+        return Nil, nil
+    }
+    var (
+        length = len(stringSlice)
+        data   = strings.Builder{}
+    )
+    data.WriteString(strconv.Itoa(length))
+    data.WriteByte(codec.Split)
+    for _, s := range stringSlice {
+        data.WriteString(strconv.Itoa(len(s)))
+        data.WriteByte(codec.Split)
+        data.WriteString(s)
+    }
+    return data.String(), nil
 }
 
 func (StringSliceSerializer) Deserialize(data string, tag string) (interface{}, error) {
-	var stringSlice StringSlice
-	if strings.EqualFold(data, Nil) {
-		return stringSlice, nil
-	}
-	index := strings.IndexByte(data, codec.Split)
-	length, err := strconv.Atoi(data[:index])
-	if err != nil {
-		return stringSlice, err
-	}
-	stringSlice = make(StringSlice, length)
-	index++
-	for i := 0; i < length; i++ {
-		next := index + strings.IndexByte(data[index:], codec.Split)
-		length, err := strconv.Atoi(data[index:next])
-		if err != nil {
-			return stringSlice, err
-		}
-		index = next + 1 + length
-		stringSlice[i] = data[next+1 : index]
-	}
-	return stringSlice, nil
+    var stringSlice StringSlice
+    if strings.EqualFold(data, Nil) {
+        return stringSlice, nil
+    }
+    index := strings.IndexByte(data, codec.Split)
+    length, err := strconv.Atoi(data[:index])
+    if err != nil {
+        return stringSlice, err
+    }
+    stringSlice = make(StringSlice, length)
+    index++
+    for i := 0; i < length; i++ {
+        next := index + strings.IndexByte(data[index:], codec.Split)
+        length, err := strconv.Atoi(data[index:next])
+        if err != nil {
+            return stringSlice, err
+        }
+        index = next + 1 + length
+        stringSlice[i] = data[next+1 : index]
+    }
+    return stringSlice, nil
 }
