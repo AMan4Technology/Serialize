409876a6551db0d0bf80fa4391e26bace2e67823
AMan <35390271+aman4technology@users.noreply.github.com>
2019-10-11 00:27:31 +0800

    feat: add StringSlice Serializer

--- internal/Base.go
+++ internal/Base.go
@@ -1,65 +1,65 @@
 package internal
 
 import (
-    "errors"
-    "fmt"
-    "reflect"
-    "strconv"
-    "strings"
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 const (
-    Int     = "int"
-    Uint    = "uint"
-    Float   = "float"
-    Complex = "complex"
+	Int     = "int"
+	Uint    = "uint"
+	Float   = "float"
+	Complex = "complex"
 )
 
 func baseSerialize(value interface{}) string {
-    return fmt.Sprint(value)
+	return fmt.Sprint(value)
 }
 
-func baseDeserialize(tp reflect.Type, data string) (interface{}, error) {
-    value, _ := baseDeserializeWith(tp.Kind().String(), data)
-    return reflect.ValueOf(value).Convert(tp).Interface(), nil
+func baseDeserialize(tp reflect.Type, data string) (value interface{}, err error) {
+	value, err = baseDeserializeWith(tp.Kind().String(), data)
+	return reflect.ValueOf(value).Convert(tp).Interface(), err
 }
 
 func baseDeserializeWith(kind, data string) (interface{}, error) {
-    switch kind {
-    case reflect.String.String():
-        return data, nil
-    case reflect.Bool.String():
-        return strconv.ParseBool(data)
-    case reflect.Int.String():
-        return strconv.Atoi(data)
-    case reflect.Int8.String(), reflect.Int16.String(), reflect.Int32.String(), reflect.Int64.String():
-        bitSize, _ := strconv.Atoi(kind[len(Int):])
-        return strconv.ParseInt(data, 0, bitSize)
-    case reflect.Float32.String(), reflect.Float64.String():
-        bitSize, _ := strconv.Atoi(kind[len(Float):])
-        return strconv.ParseFloat(data, bitSize)
-    case reflect.Uint.String(), reflect.Int8.String(), reflect.Int16.String(), reflect.Int32.String(), reflect.Int64.String():
-        bitSize, _ := strconv.Atoi(kind[len(Uint):])
-        return strconv.ParseUint(data, 0, bitSize)
-    case reflect.Complex64.String():
-        var (
-            bitSize, _ = strconv.Atoi(kind[len(Complex):])
-            tp         = Float + strconv.Itoa(bitSize/2)
-            split      = strings.LastIndexByte(data, '+') + strings.LastIndexByte(data, '-')
-            a, _       = baseDeserializeWith(tp, data[:split])
-            b, _       = baseDeserializeWith(tp, data[split:len(data)-1])
-        )
-        return complex(a.(float32), b.(float32)), nil
-    case reflect.Complex128.String():
-        var (
-            bitSize, _ = strconv.Atoi(kind[len(Complex):])
-            tp         = Float + strconv.Itoa(bitSize/2)
-            split      = strings.LastIndexByte(data, '+') + strings.LastIndexByte(data, '-')
-            a, _       = baseDeserializeWith(tp, data[:split])
-            b, _       = baseDeserializeWith(tp, data[split:len(data)-1])
-        )
-        return complex(a.(float64), b.(float64)), nil
-    default:
-        return nil, errors.New("unsupported kind:" + kind)
-    }
+	switch kind {
+	case reflect.String.String():
+		return data, nil
+	case reflect.Bool.String():
+		return strconv.ParseBool(data)
+	case reflect.Int.String():
+		return strconv.Atoi(data)
+	case reflect.Int8.String(), reflect.Int16.String(), reflect.Int32.String(), reflect.Int64.String():
+		bitSize, _ := strconv.Atoi(kind[len(Int):])
+		return strconv.ParseInt(data, 0, bitSize)
+	case reflect.Float32.String(), reflect.Float64.String():
+		bitSize, _ := strconv.Atoi(kind[len(Float):])
+		return strconv.ParseFloat(data, bitSize)
+	case reflect.Uint.String(), reflect.Int8.String(), reflect.Int16.String(), reflect.Int32.String(), reflect.Int64.String():
+		bitSize, _ := strconv.Atoi(kind[len(Uint):])
+		return strconv.ParseUint(data, 0, bitSize)
+	case reflect.Complex64.String():
+		var (
+			bitSize, _ = strconv.Atoi(kind[len(Complex):])
+			tp         = Float + strconv.Itoa(bitSize/2)
+			split      = strings.LastIndexByte(data, '+') + strings.LastIndexByte(data, '-')
+			a, _       = baseDeserializeWith(tp, data[:split])
+			b, _       = baseDeserializeWith(tp, data[split:len(data)-1])
+		)
+		return complex(a.(float32), b.(float32)), nil
+	case reflect.Complex128.String():
+		var (
+			bitSize, _ = strconv.Atoi(kind[len(Complex):])
+			tp         = Float + strconv.Itoa(bitSize/2)
+			split      = strings.LastIndexByte(data, '+') + strings.LastIndexByte(data, '-')
+			a, _       = baseDeserializeWith(tp, data[:split])
+			b, _       = baseDeserializeWith(tp, data[split:len(data)-1])
+		)
+		return complex(a.(float64), b.(float64)), nil
+	default:
+		return nil, errors.New("unsupported kind:" + kind)
+	}
 }
--- internal/StringSlice.go
+++ internal/StringSlice.go
@@ -1,4 +1,62 @@
 package internal
 
+import (
+	"reflect"
+	"strconv"
+	"strings"
+
+	"Serialize/codec"
+)
+
+func init() {
+	_ = Register(reflect.TypeOf(StringSlice{}), StringSliceSerializer{}, false)
+}
+
+const Nil = "nil"
+
 type StringSlice []string
 
+type StringSliceSerializer struct{}
+
+func (StringSliceSerializer) Serialize(value interface{}, tag string) (string, error) {
+	stringSlice := value.(StringSlice)
+	if stringSlice == nil {
+		return Nil, nil
+	}
+	var (
+		length = len(stringSlice)
+		data   = strings.Builder{}
+	)
+	data.WriteString(strconv.Itoa(length))
+	data.WriteByte(codec.Split)
+	for _, s := range stringSlice {
+		data.WriteString(strconv.Itoa(len(s)))
+		data.WriteByte(codec.Split)
+		data.WriteString(s)
+	}
+	return data.String(), nil
+}
+
+func (StringSliceSerializer) Deserialize(data string, tag string) (interface{}, error) {
+	var stringSlice StringSlice
+	if strings.EqualFold(data, Nil) {
+		return stringSlice, nil
+	}
+	index := strings.IndexByte(data, codec.Split)
+	length, err := strconv.Atoi(data[:index])
+	if err != nil {
+		return stringSlice, err
+	}
+	stringSlice = make(StringSlice, length)
+	index++
+	for i := 0; i < length; i++ {
+		next := index + strings.IndexByte(data[index:], codec.Split)
+		length, err := strconv.Atoi(data[index:next])
+		if err != nil {
+			return stringSlice, err
+		}
+		index = next + 1 + length
+		stringSlice[i] = data[next+1 : index]
+	}
+	return stringSlice, nil
+}
