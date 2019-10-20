8732081a8b86d5c56bc0d2a128aa87a125673a0e
AMan <35390271+aman4technology@users.noreply.github.com>
2019-10-21 01:22:47 +0800

    fix: register serializer for ptr type include circle

---
+++ Registry_test.go
@@ -0,0 +1,18 @@
+package serialize
+
+import (
+    "fmt"
+    "reflect"
+    "testing"
+
+    "github.com/AMan4Technology/Serialize/internal"
+)
+
+func TestRegister(t *testing.T) {
+    fmt.Println(internal.Register(reflect.TypeOf(new(node)), nil, true))
+    fmt.Println(internal.SerializerWithID)
+}
+
+type node struct {
+    next *node
+}
--- internal/Registry.go
+++ internal/Registry.go
@@ -1,26 +1,32 @@
 package internal
 
 import (
+    "errors"
     "fmt"
     "reflect"
     "strings"
 )
 
 func Register(tp reflect.Type, s Serializer, update bool) (err error) {
+    if SerializerWithID[IDOf(tp)] != nil && !update {
+        return errors.New("type has been registered")
+    }
     switch tp.Kind() {
     case reflect.Struct:
+        err = register(IDOf(tp), tp, s, update)
         for i := 0; i < tp.NumField(); i++ {
             _ = Register(tp.Field(i).Type, nil, false)
         }
     case reflect.Slice, reflect.Array:
+        err = register(IDOf(tp), tp, s, update)
         _ = Register(tp.Elem(), nil, false)
     case reflect.Map:
+        err = register(IDOf(tp), tp, s, update)
         _, _ = Register(tp.Key(), nil, false), Register(tp.Elem(), nil, false)
     case reflect.Ptr:
-        _ = Register(tp.Elem(), nil, false)
-        return
+        _ = Register(tp.Elem(), s, update)
     }
-    return register(IDOf(tp), tp, s, update)
+    return
 }
 
 func IDOf(tp reflect.Type) string {
