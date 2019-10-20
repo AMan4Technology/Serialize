c6d55d4874ddb7b090d496d8b753d9b361d1472d
AMan <35390271+aman4technology@users.noreply.github.com>
2019-10-21 01:37:31 +0800

    fix: register serializer for base type

--- Registry_test.go
+++ Registry_test.go
@@ -10,9 +10,12 @@ import (
 
 func TestRegister(t *testing.T) {
     fmt.Println(internal.Register(reflect.TypeOf(new(node)), nil, true))
+    fmt.Println(internal.Register(reflect.TypeOf(new(val)), nil, true))
     fmt.Println(internal.SerializerWithID)
 }
 
 type node struct {
     next *node
 }
+
+type val int
--- internal/Registry.go
+++ internal/Registry.go
@@ -11,20 +11,19 @@ func Register(tp reflect.Type, s Serializer, update bool) (err error) {
     if SerializerWithID[IDOf(tp)] != nil && !update {
         return errors.New("type has been registered")
     }
+    if tp.Kind() == reflect.Ptr {
+        return Register(tp.Elem(), s, update)
+    }
+    err = register(IDOf(tp), tp, s, update)
     switch tp.Kind() {
     case reflect.Struct:
-        err = register(IDOf(tp), tp, s, update)
         for i := 0; i < tp.NumField(); i++ {
             _ = Register(tp.Field(i).Type, nil, false)
         }
     case reflect.Slice, reflect.Array:
-        err = register(IDOf(tp), tp, s, update)
         _ = Register(tp.Elem(), nil, false)
     case reflect.Map:
-        err = register(IDOf(tp), tp, s, update)
         _, _ = Register(tp.Key(), nil, false), Register(tp.Elem(), nil, false)
-    case reflect.Ptr:
-        _ = Register(tp.Elem(), s, update)
     }
     return
 }
